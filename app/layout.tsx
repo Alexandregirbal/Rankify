f90fd5946d76ad889d4e9e7df24b4f3259004fae
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-08-24 21:58:04 +0200

    add icons and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Elo World",
+  title: "Rankify",
   description: "Live pool rankings at Waapi",
 };
 
