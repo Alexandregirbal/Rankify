fcb03b804054c01b096409770c56e54dbefe5a98
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-08-26 17:22:14 +0200

    update manifest to ts file

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Rankify, Waapi's pool ranking app",
+    short_name: "Rankify",
+    description: "Ranking app of the Waapi pool community",
+    start_url: "/",
+    display: "fullscreen",
+    theme_color: "#00CDB8",
+    background_color: "#ffffff",
+    icons: [
+      {
+        src: "/logo-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/logo-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/maskable-icon.png",
+        sizes: "any",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
