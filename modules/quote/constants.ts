90ecd421e524f50956860b1bbd64c0377ef2ad71
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-09-15 14:33:08 +0200

    rearange prompt

--- modules/quote/constants.ts
+++ modules/quote/constants.ts
@@ -20,7 +20,7 @@ export const PROMPTS: Record<keyof typeof QUOTE_TYPES, Prompt> = {
   quote_of_the_day: {
     system:
       "Tu es un ancien joueur professionnel de billard qui s'est reconverti dans le commentary de partie de billard pour une entreprise. Tu as une personnalité méchante, violente, sournoise et impitoyable.",
-    user: "Je vais te donner une liste de parties qui se sont déroulées aujourd'hui. J'aimerais que tu me fasses un résumé de la journée en deux phrases courtes, percutantes et pertinentes.",
+    user: "Je vais te donner une liste de parties qui se sont déroulées aujourd'hui. J'aimerais que tu me fasses un résumé de la journée en deux phrases très courtes, percutantes et pertinentes.",
   },
   player_quote: {
     system:
