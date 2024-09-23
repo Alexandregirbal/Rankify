dccc6d5bcf1b059f8df4c01c424bcd186003693f
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2024-09-23 09:28:42 +0200

    update stats with season games

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -5,23 +5,41 @@ import {
   getTotalNumberOfGames,
 } from "@/modules/game/get";
 import { getAllPlayersRatingHistories } from "@/modules/player/get";
+import { getCurrentSeason } from "@/modules/season/get";
 import dayjs from "dayjs";
 import RatingHistories from "./components/ratingHistories";
 
 export default async function Charts() {
-  const allPlayers = await getAllPlayersRatingHistories();
-  const totalNumberOfGamesPlayed = await getTotalNumberOfGames({});
-  const numberOfGamesPlayedToday = await getNumberOfGamesSince({
-    since: dayjs().startOf("day").toDate(),
-  });
+  const currentSeason = await getCurrentSeason();
+  const [
+    allPlayers,
+    totalNumberOfGamesPlayed,
+    seasonNumberOfGamesPlayed,
+    numberOfGamesPlayedToday,
+  ] = await Promise.all([
+    getAllPlayersRatingHistories(),
+    getTotalNumberOfGames({}),
+    getNumberOfGamesSince({
+      since: currentSeason?.startDate,
+    }),
+    getNumberOfGamesSince({
+      since: dayjs().startOf("day").toDate(),
+    }),
+  ]);
 
   return (
     <div className="h-full flex flex-col items-center gap-2 p-4 overflow-y-scroll text-lg">
       <h1 className="text-center text-2xl">{"Season's Stats"}</h1>
       <ul className="w-full">
         <li>
-          <span>Games played:</span>{" "}
-          <span className="font-bold">{totalNumberOfGamesPlayed}</span>
+          <span>Games played total:</span>{" "}
+          <span className="font-bold">
+            {totalNumberOfGamesPlayed.toLocaleString()}
+          </span>
+        </li>
+        <li>
+          <span>Games played this season:</span>{" "}
+          <span className="font-bold">{seasonNumberOfGamesPlayed}</span>
         </li>
         <li>
           <span>Games played today:</span>{" "}
