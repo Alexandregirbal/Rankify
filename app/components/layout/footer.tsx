4abe79a180996068597ab7638858d987d361cb9e
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2025-01-03 17:15:54 +0100

    fix: try fixing lag issue #51

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -3,39 +3,23 @@
 import { ChartNoAxesCombined, SquarePlus, Trophy } from "lucide-react";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 
 export default function Footer() {
   const pathname = usePathname();
   const params = useParams<{ activityName: string }>();
 
-  const [activeTab, setActiveTab] = useState<
-    null | "stats" | "leaderboard" | "add"
-  >(null);
-
-  useEffect(() => {
-    if (pathname.toLowerCase().endsWith("/stats")) {
-      setActiveTab("stats");
-    } else if (pathname.toLowerCase().endsWith("/add")) {
-      setActiveTab("add");
-    } else if (pathname.endsWith(params.activityName)) {
-      setActiveTab("leaderboard");
-    } else {
-      setActiveTab(null);
-    }
-
-    return () => {
-      setActiveTab(null);
-    };
-  }, [pathname, params.activityName]);
-
   const isActiveTab = (tab: string) => {
-    return activeTab === tab ? "active bg-neutral" : "";
+    const path = pathname.toLowerCase();
+    const isActive =
+      (tab === "stats" && path.endsWith("/stats")) ||
+      (tab === "add" && path.endsWith("/add")) ||
+      (tab === "leaderboard" && path.endsWith(params.activityName.toLowerCase()));
+    return isActive ? "active bg-neutral" : "";
   };
 
   const buildLink = (tab: "stats" | "leaderboard" | "add") => {
     const { activityName } = params;
-    if (!activityName) return `/`;
+    if (!activityName) return "/";
     switch (tab) {
       case "stats":
         return `/${activityName}/stats`;
@@ -50,18 +34,15 @@ export default function Footer() {
 
   return (
     <footer className="btm-nav bg-neutral">
-      <Link className={isActiveTab("stats")} href={buildLink("stats")}>
+      <Link prefetch={true} className={isActiveTab("stats")} href={buildLink("stats")}>
         <ChartNoAxesCombined />
       </Link>
 
-      <Link
-        className={isActiveTab("leaderboard")}
-        href={buildLink("leaderboard")}
-      >
+      <Link prefetch={true} className={isActiveTab("leaderboard")} href={buildLink("leaderboard")}>
         <Trophy />
       </Link>
 
-      <Link className={isActiveTab("add")} href={buildLink("add")}>
+      <Link prefetch={true} className={isActiveTab("add")} href={buildLink("add")}>
         <SquarePlus />
       </Link>
     </footer>
